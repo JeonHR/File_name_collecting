3dd8fe819dee76a5d313f46ea3a95177cfe1eea3
SEMI_ENG <140233882+JeonHR@users.noreply.github.com>
2024-02-14 17:10:08 +0900

    final change the folder name
    + from ~nxI  to `nI  to define exactly folder name 
    + Allow blanks by using the " "

--- File_name_collecting.bat
+++ File_name_collecting.bat
@@ -1,4 +1,4 @@
 @echo off
-for %%I in ("%~dp0.") do set "parent_folder=%%~nxI"
+for %%I in ("%~dp0.") do set "parent_folder=%%~nI" 
 set filename=%parent_folder%.txt
-dir /b > %filename%
+dir /b > "%filename%"
